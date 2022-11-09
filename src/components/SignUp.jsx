51bc49a2ee25d6f97cc0a436328d9d65fe650e6f
Marie Fiskbæk <marie.fiskbak@gmail.com>
2022-11-09 10:30:12 +0100

    sign for clears after sign up

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -6,10 +6,12 @@ function SignUp(props) {
     const [signUpInfo, setSignUpInfo] = useState(init);
 
 
+
     const createUser = (evt) => {
         evt.preventDefault();
         if (signUpInfo.password === signUpInfo.rPassword) {
             facade.createUser(signUpInfo.username, signUpInfo.password, signUpInfo.rPassword);
+            setSignUpInfo({username: "", password: "", rPassword: ""})
         } else {
             console.log("noget gik galt") //TODO, giv brugeren en besked
         }
@@ -23,9 +25,9 @@ function SignUp(props) {
         <div>
             <h2>Sign up</h2>
             <form>
-                <input onChange={onChange} type="text" placeholder="User Name" id="username"/>
-                <input onChange={onChange} type="text" placeholder="Password" id="password"/>
-                <input onChange={onChange} type="text" placeholder="Repeat password" id="rPassword"/>
+                <input onChange={onChange} type="text" placeholder="User Name" id="username" value={signUpInfo.username}/>
+                <input onChange={onChange} type="text" placeholder="Password" id="password" value={signUpInfo.password}/>
+                <input onChange={onChange} type="text" placeholder="Repeat password" id="rPassword" value={signUpInfo.rPassword}/>
                 <button onClick={createUser} type="submit">Sign Up</button>
             </form>
         </div>
